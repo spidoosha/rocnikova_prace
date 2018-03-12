c377154a9b6ae047ed31f867161e93118a3480c6
spidoosha <36747949+spidoosha@users.noreply.github.com>
2018-03-12 20:03:28 +0100

    Add files via upload

---
+++ rocnikova_prace/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnDisable = new System.Windows.Forms.Button();
+            this.BtnEnable = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hi";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Text me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.EnterTheInput);
+            // 
+            // BtnDisable
+            // 
+            this.BtnDisable.Enabled = false;
+            this.BtnDisable.Location = new System.Drawing.Point(132, 426);
+            this.BtnDisable.Name = "BtnDisable";
+            this.BtnDisable.Size = new System.Drawing.Size(120, 23);
+            this.BtnDisable.TabIndex = 8;
+            this.BtnDisable.Text = "Disable Voice";
+            this.BtnDisable.UseVisualStyleBackColor = true;
+            this.BtnDisable.Click += new System.EventHandler(this.BtnDisable_Click);
+            // 
+            // BtnEnable
+            // 
+            this.BtnEnable.Location = new System.Drawing.Point(12, 426);
+            this.BtnEnable.Name = "BtnEnable";
+            this.BtnEnable.Size = new System.Drawing.Size(114, 23);
+            this.BtnEnable.TabIndex = 9;
+            this.BtnEnable.Text = "Enable Voice";
+            this.BtnEnable.UseVisualStyleBackColor = true;
+            this.BtnEnable.Click += new System.EventHandler(this.BtnEnable_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(434, 382);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 461);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnEnable);
+            this.Controls.Add(this.BtnDisable);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dialogflow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnDisable;
+        private System.Windows.Forms.Button BtnEnable;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ rocnikova_prace/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,288 @@
+﻿/*
+ * ROČNÍKOVÁ PRÁCE
+ * DIALOGFLOW
+ * I S PO
+ * HOANG ANH TUAN, 8.M
+ * PROGRAMOVACÍ SEMNINÁŘ II 2017/2018
+*/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ApiAiSDK;
+using ApiAiSDK.Model;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+/*
+ * 
+ * Nainstaloval jsem si knihovnu api.ai .NET pomocí Nugetu - Package Manager Console příkazem "PM> Install-Package ApiAiSDK"
+ * Nadefinoval jsem si ji pomocí: 
+ * - using ApiAiSDK
+ * - using ApiAiSDK.Model
+ * Dále jsem nadefinoval knihovnu, pomocí které může moje aplikace "mluvit" a já můžu mluvit do ní:
+ * - using System.Speech.Recognition
+ * - using System.Speech.Synthesis
+ * 
+*/
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static ApiAi apiAi;
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        // Vytvořil jsem si třídu Sandwich. Každý Sandwich má omáčku, 1-3 ingredience a počáteční nulovou cenu
+        // K tomu list, do které si budu zapisovat všechny sendviče a int celkové ceny
+        class Sandwich
+        {
+            public string sauce;
+            public string ing;
+            public string ing1;
+            public string ing2;
+            public int cena = 0;
+        }
+        List<Sandwich> list = new List<Sandwich>();
+        int celkovaCena = 0;
+        string odpoved;
+        //Do této funkce vstupuje to, co nám uživatel řekne a rozhoduje, co aplikace dále odpoví
+        void PrijmiOdpovedAOdpovez(string input)
+        {
+            // vkládáme do AIConfiguration token našeho agenta a oznamujeme mu, že konverzace probíhá v angličtině
+            var config = new AIConfiguration("bfd6b02ea0ac450e9bb859c2ed1a1760", SupportedLanguage.English);
+            apiAi = new ApiAi(config);
+
+            // zde vkládáme vstup uživatele
+            var vstup = apiAi.TextRequest(input);
+            richTextBox1.Text += "You: " + input + '\n';
+            // errorx;
+            // PripravSandwich(vstup.Result.Parameters["Ingredients[0]"].ToString(), vstup.Result.Parameters["sauce"].ToString(), new Sandwich()); 
+            Sandwich sendvic = new Sandwich();
+            if (vstup.Result.Metadata.IntentName == "order_sandwich" || vstup.Result.Metadata.IntentName == "order_new_sandwich")
+            {
+                // Ingredience se musejí z vstup.Result.Parameters["Ingredients"].ToString() vyextrahovat
+                PripravSandwich(vstup.Result.Parameters["Ingredients"].ToString(), vstup.Result.Parameters["sauce"].ToString(), sendvic);
+                celkovaCena = celkovaCena + sendvic.cena;
+                list.Add(sendvic);
+
+                // Agent zrekapituluje poslední zadaný sendvič
+                if (vstup.Result.ActionIncomplete == false)
+                {
+                    odpoved = "You have ordered " + sendvic.sauce + " sandwich with " + sendvic.ing + " " + sendvic.ing1 + " " + sendvic.ing2 + ". It will cost " + sendvic.cena + " Dialogflow coins. Would you like to get another one?";
+                    synthesizer.SpeakAsync(odpoved);
+                    richTextBox1.Text += "Agent: " + odpoved + '\n';
+                }
+                else
+                {
+                    synthesizer.SpeakAsync(vstup.Result.Fulfillment.Speech);
+                    richTextBox1.Text += "Agent: " + vstup.Result.Fulfillment.Speech + '\n';
+                }
+            }
+            // Agent zrekapituluje celou objednavku
+            else if (vstup.Result.Metadata.IntentName == "say_goodbye")
+            {
+                
+                odpoved = "You have ordered " + VypisSandwiche(list) + "It's " + celkovaCena + " Dialogflow coins.";
+                synthesizer.SpeakAsync(odpoved);
+                richTextBox1.Text += "Agent: " + odpoved + '\n';
+                celkovaCena = 0;
+            }
+            else
+            {
+                // Odpoved je výchozí podle agenta
+                synthesizer.SpeakAsync(vstup.Result.Fulfillment.Speech);
+                richTextBox1.Text += "Agent: " + vstup.Result.Fulfillment.Speech + '\n';
+            }
+        }
+
+        // Vypíše všechny sendviče
+        static string VypisSandwiche(List<Sandwich> kredenc)
+        {
+            int i = 0;
+            string vypis = "";
+            foreach (Sandwich s in kredenc)
+            {
+                if (i < kredenc.Count)
+                {
+                    vypis += s.sauce + " sandwich with " + s.ing + " " + s.ing1 + " " + s.ing2 + ", ";
+                }
+                else
+                {
+                    vypis += "and " + s.sauce + " sandwich with " + s.ing + " " + s.ing1 + " " + s.ing2;
+                }
+                i++;
+            }
+
+            return vypis;
+        }
+
+        //Do Sandwich sandwich přidá hodnoty, nejdřív si musí ale vyextrahovat ingredience
+        void PripravSandwich(string ingredience, string sauce, Sandwich sandwich)
+        {
+            if (sauce != "")
+                sandwich.sauce = sauce;
+            sandwich.cena = sandwich.cena + VypocitejUtratuOmacky(sandwich.sauce);
+            bool zacni = false;
+
+            bool konec = false;
+            int NtaIng = 0;
+            string ing = "";
+            if (ingredience != "")
+                foreach (char c in ingredience)
+                {
+                    if (zacni == true)
+                    {
+                        if (c == '"')
+                        {
+                            if (NtaIng == 0)
+                            {
+                                sandwich.ing = ing;
+                                sandwich.cena = sandwich.cena + VypocitejUtratuIng(sandwich.ing);
+                                NtaIng++;
+                            }
+                            else if (NtaIng == 1)
+                            {
+                                sandwich.ing1 = ing;
+                                sandwich.cena = sandwich.cena + VypocitejUtratuIng(sandwich.ing1);
+                                NtaIng++;
+                            }
+                            else if (NtaIng == 2)
+                            {
+                                sandwich.ing2 = ing;
+                                sandwich.cena = sandwich.cena + VypocitejUtratuIng(sandwich.ing2);
+                            }
+
+                            ing = "";
+                            zacni = false;
+                            konec = true;
+                        }
+                        else
+                        {
+                            ing = ing + c;
+                        }
+                    }
+
+                    if (c == '"')
+                    {
+                        if (konec == true)
+                        {
+                            konec = false;
+                        }
+                        else
+                        {
+                            zacni = true;
+                        }
+                    }
+                }
+            NtaIng = 0;
+
+        }
+        
+        //Spočítá ceny omacky a ingredienci
+        static int VypocitejUtratuOmacky(string sauce)
+        {
+            int utrata = 0;
+            switch (sauce)
+            {
+                case "mayo":
+                    utrata = 2;
+                    break;
+                case "ketchup":
+                    utrata = 2;
+                    break;
+                case "chilli":
+                    utrata = 3;
+                    break;
+                case "mustard":
+                    utrata = 1;
+                    break;
+            }
+
+            return utrata;
+        }
+        static int VypocitejUtratuIng(string ing)
+        {
+            int utrata = 0;
+            switch (ing)
+            {
+                case "salami":
+                    utrata = 3;
+                    break;
+                case "cheese":
+                    utrata = 5;
+                    break;
+                case "salad":
+                    utrata = 3;
+                    break;
+                case "stripsy":
+                    utrata = 2;
+                    break;
+                case "tomatoes":
+                    utrata = 1;
+                    break;
+                case "cucumber":
+                    utrata = 2;
+                    break;
+                case "butter":
+                    utrata = 5;
+                    break;
+                case "ham":
+                    utrata = 5;
+                    break;
+                case "eggs":
+                    utrata = 5;
+                    break;
+                default:
+                    utrata = 0;
+                    break;
+            }
+            return utrata;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Zpracuje textový vstup
+        private void EnterTheInput(object sender, EventArgs e)
+        {
+            string input;
+            input = textBox1.Text;
+            PrijmiOdpovedAOdpovez(input);
+            textBox1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognised;
+        }
+
+        //Hlasovy vstup se zkomprimuje do textove podoby
+        void recEngine_SpeechRecognised(object sender, SpeechRecognizedEventArgs e)
+        {
+            PrijmiOdpovedAOdpovez(e.Result.Text);
+        }
+        // Po kliknutí na "Enable Voice" se začne nahrávat
+        private void BtnEnable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            BtnDisable.Enabled = true;
+        }
+        // Po kliknutí na "Disable Voice" se přestane nahrávat
+        private void BtnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            BtnDisable.Enabled = false;
+        }
+    }
+}
